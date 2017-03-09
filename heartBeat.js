3e041f22834521fa5cb0dea5f5d7a78a169e7f5a
mayiwen <472953586@qq.com>
2017-03-09 17:20:33 +0800

    避免重连两次

--- heartBeat.js
+++ heartBeat.js
@@ -57,7 +57,7 @@
                 //onmessage拿到返回的心跳就说明连接正常
                 ws.send("HeartBeat");
                 self.serverTimeoutObj = setTimeout(function(){//如果超过一定时间还没重置，说明后端主动断开了
-                    reconnect();
+                    ws.close();//如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
                 }, self.timeout)
             }, this.timeout)
         }

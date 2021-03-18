e547ca719d512e8682b9c1155240166270d8293c
zimv <472953586@qq.com>
2021-03-18 14:36:07 +0800

    fix: window error in nodejs

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-
 const path = require('path');
 const config = {
-    entry: {
-        index: './lib/index.js'
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    optimization: {
-        minimize: true
-    },
-    module:{
-        rules:[
-            {
-                test: /\.js$/,
-                exclude: /node_modules/, 
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    mode: 'production'
+  entry: {
+    index: './lib/index.js',
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
+  },
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'production',
 };
 module.exports = config;
